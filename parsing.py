8b8f2728ef2749817975062b79e8caac076a0a52
Kamil Nguyen Van <knguyen@zinio.com>
2017-05-12 00:43:39 +0200

    Add readme

---
+++ parsing.py
@@ -0,0 +1,12 @@
+import re
+
+p = re.compile('[^\S\r\n]')
+
+def addApoAndComa(s):
+    splitted = s.split("\n")
+    for i in range(len(splitted) - 1):
+        splitted[i] = p.match(splitted[i])
+        print("\"" + splitted[i] + "\"," )
+    print("\"" + splitted[len(splitted) - 1] + "\"")
+
+addApoAndComa("bell")
