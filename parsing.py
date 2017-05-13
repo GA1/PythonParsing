4316987bf6c3353e16abe5a2fab3988d7acc5a7d
GA1 <nvkga1@gmail.com>
2017-05-13 19:35:53 +0200

    Finish put apostrophes and comas for json array elements

--- parsing.py
+++ parsing.py
@@ -1,11 +1,29 @@
-import re
-
-
 def addApoAndComa(s):
     splitted = s.split("\n")
     for i in range(len(splitted) - 1):
-        splitted[i] = p.match(splitted[i])
-        print("\"" + splitted[i] + "\"," )
-    print("\"" + splitted[len(splitted) - 1] + "\"")
+        print("\"" + splitted[i].strip() + "\"," )
+    print("\"" + splitted[len(splitted) - 1].strip() + "\"")
 
-addApoAndComa("bell")
+s = """          bell
+          Sigismund [name]
+          until
+          recently
+          to be
+          the biggest
+          in
+          Poland
+          until
+          in 
+          year
+          1999
+          to create/to make
+          still/even
+          bigger
+          bell
+          which 
+          to be called
+          bell
+          Marie
+          Mother of God"""
+
+addApoAndComa(s)
