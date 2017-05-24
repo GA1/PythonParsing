0837fec2b52655a7fc8b4c512b041c75fdb83afb
GA1 <nvkga1@gmail.com>
2017-05-24 11:30:58 +0200

    Filter every 10nth line

---
+++ file-shortener.py
@@ -0,0 +1,14 @@
+from_file_name = 'big_file.txt'
+result_file_name = 'file_with_every_nth_line.txt'
+extract_frequency = 100
+
+
+with open(from_file_name) as from_file:
+    lines = from_file.readlines()
+    result_file = open(result_file_name, 'w')
+    result_lines = lines[0::10]
+    for line in result_lines:
+        result_file.write(line)
+
+from_file.close()
+result_file.close()
