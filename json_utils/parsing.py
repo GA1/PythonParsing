efa6ec6bf33be7444514b65859fd030dcfc92894
GA1 <nvkga1@gmail.com>
2017-05-28 13:49:53 +0200

    Parse whole sentences

--- json_utils/parsing.py
+++ json_utils/parsing.py
@@ -1,14 +1,25 @@
-
+import re
 
 
 def add_apostrophes_and_comas(s):
     split = [s.strip() for s in s.split("\n")]
+    print("{")
+    print("\"words\":[" )
     for i in range(len(split) - 1):
         print("\"" + split[i] + "\"," )
     print("\"" + split[len(split) - 1] + "\"")
-
+    print("]," )
+    print("\"words_translation\":[" )
+    for i in range(len(split) - 1):
+    	print("\"\"," )
+    print("\"\"")
+    print("]," )
+    print("\"translation\":\"\"" )
+    print("}")
 
 with open('input.txt') as f:
     data = f.read().replace('\n', ' ')
-    joint = '\n'.join(data.split())
-    add_apostrophes_and_comas(joint)
+    sentences = re.compile('[\.\!\?]').split(data)
+    for sentence in sentences:
+    	joint = '\n'.join(sentence.split())
+    	add_apostrophes_and_comas(joint)
