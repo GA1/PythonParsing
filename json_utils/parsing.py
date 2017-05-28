877681508c85f838a026201099e9b282c5c07c65
GA1 <nvkga1@gmail.com>
2017-05-28 14:17:56 +0200

    Save to ouput file

--- json_utils/parsing.py
+++ json_utils/parsing.py
@@ -3,23 +3,29 @@ import re
 
 def add_apostrophes_and_comas(s):
     split = [s.strip() for s in s.split("\n")]
-    print("{")
-    print("\"words\":[" )
+    json_sentence = ''
+    json_sentence = json_sentence + "{\n"
+    json_sentence = json_sentence + "\"words\":[\n"
     for i in range(len(split) - 1):
-        print("\"" + split[i] + "\"," )
-    print("\"" + split[len(split) - 1] + "\"")
-    print("]," )
-    print("\"words_translation\":[" )
+        json_sentence = json_sentence + "\"" + split[i] + "\",\n"
+    json_sentence = json_sentence + "\"" + split[len(split) - 1] + "\"\n"
+    json_sentence = json_sentence + "],\n"
+    json_sentence = json_sentence + "\"words_translation\":[\n"
     for i in range(len(split) - 1):
-    	print("\"\"," )
-    print("\"\"")
-    print("]," )
-    print("\"translation\":\"\"" )
-    print("}")
+        json_sentence = json_sentence + "\"\",\n"
+    json_sentence = json_sentence + "\"\"\n"
+    json_sentence = json_sentence + "],\n"
+    json_sentence = json_sentence + "\"translation\":\"\"\n"
+    json_sentence = json_sentence + "}\n"
+    return json_sentence
 
-with open('input.txt') as f:
-    data = f.read().replace('\n', ' ')
-    sentences = re.compile('[\.\!\?]').split(data)
+with open('input.txt') as input_file:
+    output = open('output.txt', 'w')
+    data = input_file.read().replace('\n', ' ')
+    sentences = re.compile('[.!\?]').split(data)
+    json_sentences = []
     for sentence in sentences:
-    	joint = '\n'.join(sentence.split())
-    	add_apostrophes_and_comas(joint)
+        joint = '\n'.join(sentence.split())
+        json_sentence = add_apostrophes_and_comas(joint)
+        json_sentences.append(json_sentence)
+    output.write('[' + ','.join(json_sentences) + ']')
